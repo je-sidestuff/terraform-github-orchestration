0b52849d4fe633fa1c9d1a084cc62f6fc4258866
je-sidestuff <gerald.halimak@gmail.com>
2025-03-19 21:24:25 +0000

    feat: Add initial scaffolding support.

--- modules/terragrunt/scaffolder/variables.tf
+++ modules/terragrunt/scaffolder/variables.tf
@@ -19,6 +19,7 @@ EOF
         branch = optional(string, "main") # TODO In the future we'll do tags
         description = optional(string)
         vars = optional(map(string), {})
+        var_file_strings = optional(list(string), [])
         var_files = optional(list(string), [])
         placement = map(string)
     })
